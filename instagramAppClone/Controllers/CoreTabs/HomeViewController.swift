828839eccb2d7263b73cd0cf37518677ae0361c6
Onore849 <rion3376@gmail.com>
2020-08-25 15:00:56 +0900

    create funcion which connects loginVC if login status is not

--- instagramAppClone/Controllers/CoreTabs/HomeViewController.swift
+++ instagramAppClone/Controllers/CoreTabs/HomeViewController.swift
@@ -7,24 +7,37 @@
 //
 
 import UIKit
+import Firebase
 
 class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        handleNotAutenticated()
+    }
+    
+    private func handleNotAutenticated() {
+        
+        if Auth.auth().currentUser == nil {
+            
+            let loginVC = LoginViewController()
+            loginVC.modalPresentationStyle = .fullScreen
+            
+            present(loginVC, animated: false)
+            
+        }
+        
+        
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
