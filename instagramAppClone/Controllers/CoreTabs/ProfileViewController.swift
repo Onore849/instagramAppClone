b4c805f6f01eeb4c95469142cc1a1a2de7163832
Onore849 <rion3376@gmail.com>
2020-08-27 16:45:29 +0900

    create collectionView in ProfileVC

--- instagramAppClone/Controllers/CoreTabs/ProfileViewController.swift
+++ instagramAppClone/Controllers/CoreTabs/ProfileViewController.swift
@@ -8,14 +8,37 @@
 
 import UIKit
 
+/// profile view controller
 final class ProfileViewController: UIViewController {
 
+    // collection Viewの設定
+    private var collectionView: UICollectionView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
         
         configureNavigationBar()
+        
+        
+        // collectionViewの設定
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.itemSize = CGSize(width: view.width/3, height: view.width/3)
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
+        
+        guard let collectionView = collectionView else {
+            
+            return
+            
+        }
+        
+        view.addSubview(collectionView)
     }
     
 
@@ -33,4 +56,25 @@ final class ProfileViewController: UIViewController {
         navigationController?.pushViewController(vc, animated: true)
         
     }
+}
+
+extension ProfileViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 0
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        return UICollectionViewCell()
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        collectionView.deselectItem(at: indexPath, animated: true)
+        
+    }
+
 }
